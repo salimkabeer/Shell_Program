97a48a1ceeaf6cd2c5ff2ee94a47827bba01a35c
salimkabeer <salimkabeer@gmail.com>
2019-06-09 07:50:08 +0530

    file and loop

---
+++ 7_forLoops.sh
@@ -0,0 +1,31 @@
+# Example 1
+for i in 1 2 3 4 5 
+do
+	echo "$i times"
+done
+
+# Example 2
+for i in {1..5}
+do
+	echo $i
+done
+
+# Example 3
+echo "Bash version : ${BASH_VERSION}..."
+for i in {0..10..2}
+do
+	echo $i
+done
+
+# Example 4
+for i in $(ls)
+do
+	echo $i
+done
+
+
+# Example 
+#for (( c=1; c<=5; c++ ))
+#do
+#	echo $c
+#done
